c40e660d70c8a1ba0f93991c57a9bf9ba59f4d46
Haruhito Fuji <haruhito.fuji@gmail.com>
2023-09-06 21:06:02 +0900

    WKYTPlayerViewDelegate部分を実装

--- YoutubePlayerTest/YoutubePlayer/YoutubePlayerView.swift
+++ YoutubePlayerTest/YoutubePlayer/YoutubePlayerView.swift
@@ -10,10 +10,45 @@ import YoutubePlayer_in_WKWebView
 
 struct YoutubePlayerView: UIViewRepresentable {
     
-    var videoID: String
+    let videoID: String
+    
+    private let playerView = WKYTPlayerView()
+    
+    class Coordinator: NSObject, WKYTPlayerViewDelegate {
+        let parent: YoutubePlayerView
 
+        init(_ parent: YoutubePlayerView) {
+            self.parent = parent
+        }
+        
+        func playerViewDidBecomeReady(_ playerView: WKYTPlayerView) {
+            print("playerViewDidBecomeReady")
+        }
+        
+        func playerView(_ playerView: WKYTPlayerView, didChangeTo state: WKYTPlayerState) {
+            print("PlayState \(state)")
+        }
+        
+        func playerView(_ playerView: WKYTPlayerView, didChangeTo quality: WKYTPlaybackQuality) {
+            print("PlayQuality \(quality)")
+        }
+        
+        func playerView(_ playerView: WKYTPlayerView, receivedError error: WKYTPlayerError) {
+            print("PlayError \(error)")
+        }
+        
+        func playerView(_ playerView: WKYTPlayerView, didPlayTime playTime: Float) {
+            print("PlayTime \(playTime)")
+        }
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        return Coordinator(self)
+    }
+    
     func makeUIView(context: Context) -> WKYTPlayerView {
-        return WKYTPlayerView()
+        playerView.delegate = context.coordinator
+        return playerView
     }
     
     func updateUIView(_ uiView: WKYTPlayerView, context: Context) {
